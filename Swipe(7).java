67b8d0e4b5de39a906b5c1cb412a4181f80bd560
Daniil Zaitsau <44928584+daden4@users.noreply.github.com>
2019-02-09 15:47:40 +0300

    Create Swipe(7).java

---
+++ Swipe(7).java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class JavaTasks {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("Enter two numbers");
+        int numA = Integer.parseInt(reader.readLine());
+        int numB = Integer.parseInt(reader.readLine());
+
+        System.out.println("Before swipe: " + numA + " " + numB);
+
+        numA += numB;
+        numB = numA - numB;
+        numA -= numB;
+
+        System.out.println("After swipe: " + numA + " " + numB);
+    }
+}

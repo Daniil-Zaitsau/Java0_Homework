e740f2bd68085afe56eb8c40bf7ca08b2afcd1cd
Daniil Zaitsau <44928584+daden4@users.noreply.github.com>
2019-02-21 12:27:35 +0300

    Update Friendly(4).java

--- Homework2/Friendly(4).java
+++ Homework2/Friendly(4).java
@@ -2,7 +2,6 @@ import java.util.Scanner;
 
 public class Friendly {
     public static void main(String[] args)   {
-        System.out.println(dividers(144));
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter start point of diapason :");
         int first = scanner.nextInt();
@@ -13,30 +12,21 @@ public class Friendly {
         if(first > last){
             System.out.println("Incorrect input");
         }else {
-            System.out.println("Friendly pairs:");
-            int memory = 0;
-            while (first < last){
-                int sum1 = dividers(first);
-                int sum2 = dividers(sum1);
-                if (sum2 == first & first != sum1 & memory != sum2) {
-                        System.out.println(first + " " + sum1);
-                        memory = sum1;
+            for (int k = first; k <= last; k++){     
+                for (int j = k+1; j <= last; j++){
+                    if(dividers(k) == j && dividers(j) == k){
+                        System.out.println(k + " " + j);
+                    }
                 }
-                first++;
             }
         }
     }
 
-    private static int dividers(int n){
-        int sum = 1;
-        for (int i = 2; i <= Math.sqrt(n); i++) {
-            if (n % i == 0) {
-                sum += i;
-                if (n / i != i) {
-                    sum += n / i;
-               }
-            }
-        }
+    private static int dividers(int x){
+        int sum = 0;
+        for(int i = 1; i <= x/2;i++)
+            if(x % i == 0)  sum += i;
         return sum;
     }
 
+}

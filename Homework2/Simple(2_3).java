3f5c2d37fa1ccc1a6753a56bd7e5dc381110eb9d
Daniil Zaitsau <44928584+daden4@users.noreply.github.com>
2019-02-22 12:08:15 +0300

    Update Simple(2_3).java

--- Homework2/Simple(2_3).java
+++ Homework2/Simple(2_3).java
@@ -1,18 +1,31 @@
 import java.util.Scanner;
 
 public class Simple {
-    public static void main(String[] args)   {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter the number :");
-        int num = scanner.nextInt();
-        int count = 0;
-
-        for (int i = 1; i <= num; i++){
-            if(num%i == 0 ) count++;
+        int num = 0;
+        try {
+            num = scanner.nextInt();
+        } catch (Exception e) {
+            System.out.println("Enter correct number!");
+            System.exit(0);
         }
-        
-        if (count == 2){
-            System.out.println("Is simple number");
+
+        if (num <= 0) {
+            System.out.println("It isn't simple number.");
+        } else {
+            int count = 1;
+
+            for (int i = 1; i <= num/2; i++) {
+                if (num % i == 0) count++;
+            }
+
+            if (count == 2) {
+                System.out.println("Is simple number");
+            } else {
+                System.out.println("It isn't simple number.");
+            }
         }
     }
 }

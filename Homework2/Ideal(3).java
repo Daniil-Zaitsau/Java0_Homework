eb737ac83e2c784f389c434b5b401164fb7c0bf5
Daniil Zaitsau <44928584+daden4@users.noreply.github.com>
2019-02-22 17:36:52 +0300

    Update Ideal(3).java

--- Homework2/Ideal(3).java
+++ Homework2/Ideal(3).java
@@ -1,20 +1,32 @@
 import java.util.Scanner;
 
-public class Ideal {
+public class JavaTasks {
     public static void main(String[] args)   {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter the number :");
-        int num = scanner.nextInt();
-        int sum = 0;
+        int num = 0;
 
-        for (int i = 1; i < num; i++){
-            if(num%i == 0 ) sum+=i;
+        try {
+            num = scanner.nextInt();
+        } catch (Exception e){
+            System.out.println("Incorrect input!");
+            System.exit(0);
         }
-
-        if(sum == num){
-            System.out.println("Ideal number.");
+        
+        if (num <=0){
+            System.out.println("Isn't natural.");
         } else {
-            System.out.println("Not ideal number.");
+            int sum = 0;
+
+            for (int i = 1; i < num; i++) {
+                if (num % i == 0) sum += i;
+            }
+
+            if (sum == num) {
+                System.out.println("Ideal number.");
+            } else {
+                System.out.println("Not ideal number.");
+            }
         }
     }
 }

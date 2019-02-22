05552394d1e3302ec39af151a247ea0889ca364a
Daniil Zaitsau <44928584+daden4@users.noreply.github.com>
2019-02-22 12:01:46 +0300

    Update Palindrom(2_2).java

--- Homework2/Palindrom(2_2).java
+++ Homework2/Palindrom(2_2).java
@@ -1,15 +1,45 @@
 import java.util.Scanner;
 
-public class Palindrom {
+public class JavaTasks {
+
     public static void main(String[] args)   {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter the number :");
-        String num = scanner.next();
 
-        StringBuilder sb = new StringBuilder(num);
-
-        if(num.equals(sb.reverse().toString())){
-            System.out.println("Is palindrom");
+        int num = 0;
+        try {
+             num = scanner.nextInt();
+        } catch (Exception e){
+            System.out.println("Please, enter correct number.");
+            System.exit(0);
         }
+
+        num = Math.abs(num);
+
+        System.out.println("First method:");
+        System.out.println(strBuild(num) ? "Is palindrom.\n" : "It isn't palindrom.\n");
+
+        System.out.println("Second method:");
+        System.out.println(palindrom(num) ? "Is palindrom.\n" : "It isn't palindrom.\n");
+
+    }
+
+    private static boolean strBuild(int num){
+        String str = String.valueOf(num);
+        StringBuilder sb = new StringBuilder(str);
+        return str.equals(sb.reverse().toString());
+    }
+
+    private static boolean palindrom(int num){
+        String str = String.valueOf(num);
+        boolean isPalin = true;
+       
+        for (int i = 0; i < str.length() & isPalin; i++){
+            if (str.charAt(i) != str.charAt(str.length() - 1 - i)){
+                isPalin = false;
+            }
+        }
+
+        return isPalin;
     }
 }

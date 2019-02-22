bbb7f3eeedaaa9ff0b178b7afcdcecb71a9085cc
Daniil Zaitsau <44928584+daden4@users.noreply.github.com>
2019-02-22 12:30:53 +0300

    Update NOD(2_5).java

--- Homework2/NOD(2_5).java
+++ Homework2/NOD(2_5).java
@@ -1,17 +1,31 @@
 import java.util.Scanner;
 
 public class Nod {
-    public static void main(String[] args)   {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter two numbers :");
-        int numA = scanner.nextInt();
-        int numB = scanner.nextInt();
+        int numA = 0;
+        int numB = 0;
 
-        int minNum = (numA > numB) ? numB : numA;
-        int nod = 0;
-        for (int i = 1; i <= minNum; i++){
-            if (numA%i == 0 && numB%i == 0) nod = i;
+        try {
+            numA = scanner.nextInt();
+            numB = scanner.nextInt();
+        } catch (Exception e) {
+            System.out.println("Incorrect input!");
+            System.exit(0);
         }
-        System.out.println("NOD is " + nod + ". NOK is " + numA*numB/nod + ".");
+
+        if (numA <= 0 || numB <= 0) {
+            System.out.println("Isn't natural numbers.");
+        } else {
+            int minNum = (numA > numB) ? numB : numA;
+            int nod = 0;
+            for (int i = 1; i <= minNum; i++) {
+                if (numA % i == 0 && numB % i == 0) {
+                    nod = i;
+                }
+            }
+            System.out.println("NOD is " + nod + ". NOK is " + numA * numB / nod + ".");
+        }
     }
 }

e675e05b8282934824be4e49ed57f743fafb42ab
Daniil Zaitsau <44928584+daden4@users.noreply.github.com>
2019-02-22 17:10:00 +0300

    Update DifferentDig(2_6).java

--- Homework2/DifferentDig(2_6).java
+++ Homework2/DifferentDig(2_6).java
@@ -4,14 +4,54 @@ public class Different {
     public static void main(String[] args)   {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter the number :");
-        String num = scanner.next();
+        int num = 0;
 
+        try {
+            num = scanner.nextInt();
+        } catch (Exception e){
+            System.out.println("Incorrect input!");
+        }
 
+        if (num <= 0){
+            System.out.println("Isn't natural number.");
+        } else {
+            System.out.println("First method:");
+            System.out.println("There are " + strMethods(num) + " different digits.\n");
+            System.out.println("Second method:");
+            System.out.println("There are " + notStrMethod(num) + " different digits.");
+        }
+    }
+
+    private static int strMethods(int num){
+        String str = String.valueOf(num);
         int count = 0;
 
         for (int i = 0; i < 10; i++){
-            if(num.contains(String.valueOf(i))) count++;
+            if(str.contains(String.valueOf(i))) count++;
         }
-        System.out.println("There are " + count + " different digits.");
+
+        return count;
+    }
+
+    private static int notStrMethod(int num){
+        
+        int count = 0;
+        int saveNum = num;
+        while (num > 0) {
+            int locNum = saveNum;
+            int digit = num % 10;
+            count++;
+            while (locNum > 0) {
+                if (digit == locNum % 10) {
+                    if(locNum > num){
+                        locNum = 0;
+                        count--;
+                    }
+                }
+                locNum = locNum / 10;
+            }
+            num = num / 10;
+        }
+        return count;
     }
 }

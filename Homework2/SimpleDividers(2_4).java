576643c92efddac56772c47b26370800ce6282d1
Daniil Zaitsau <44928584+daden4@users.noreply.github.com>
2019-02-22 12:22:11 +0300

    Update SimpleDividers(2_4).java

--- Homework2/SimpleDividers(2_4).java
+++ Homework2/SimpleDividers(2_4).java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class JavaTasks {
+public class SimpleDividers {
     public static void main(String[] args)   {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter the number :");

2c3121b237355da6ed8e70b5065ecb74a2978505
Daniil Zaitsau <44928584+daden4@users.noreply.github.com>
2019-02-22 11:34:52 +0300

    Update HeadsOrTails(1).java

--- Homework2/HeadsOrTails(1).java
+++ Homework2/HeadsOrTails(1).java
@@ -1,28 +1,38 @@
 import java.util.Random;
 import java.util.Scanner;
 
-public class Coin {
-    public static void main(String[] args)   {
+public class JavaTasks {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
         System.out.println("Enter the number of throws:");
-        int coin = scanner.nextInt();
-        
-        int i = 0;
-        int headCount = 0;
-        int tailCount = 0;
-        
-        while (i < coin){
-            int num = random.nextInt();
-            if (num % 2 == 0){
-                headCount++;
-            } else {
-                tailCount++;
-            }
-            i++;
+        int coin = 0;
+
+        try {
+            coin = scanner.nextInt();
+        } catch (Exception e) {
+            System.out.println("Enter correct number!");
+            System.exit(0);
         }
 
-        System.out.println("There are " + headCount + " heads and " + tailCount + " tails.");
+        if (coin <= 0) {
+            System.out.println("Incorrect input!");
+        } else {
+            int i = 0;
+            int headCount = 0;
+            int tailCount = 0;
 
+            while (i < coin) {
+                int num = random.nextInt();
+                if (num % 2 == 0) {
+                    headCount++;
+                } else {
+                    tailCount++;
+                }
+                i++;
+            }
+
+            System.out.println("There are " + headCount + " heads and " + tailCount + " tails.");
+        }
     }
 }

92c83a0d573e6e2743b3c01cbe76328d3ab651d2
Daniil Zaitsau <44928584+daden4@users.noreply.github.com>
2019-02-22 12:21:48 +0300

    Update SimpleDividers(2_4).java

--- Homework2/SimpleDividers(2_4).java
+++ Homework2/SimpleDividers(2_4).java
@@ -1,13 +1,39 @@
 import java.util.Scanner;
 
-public class SimpleDividers {
+public class JavaTasks {
     public static void main(String[] args)   {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter the number :");
-        int num = scanner.nextInt();
 
-        for (int i = 1; i <= num; i++){
-            if(num%i == 0 ) System.out.println(i + " is simple divider.");
+        int num = 0;
+        try {
+             num = scanner.nextInt();
+        } catch (Exception e){
+            System.out.println("Incorrect input!");
+            System.exit(0);
+        }
+
+        if (num <= 0){
+            System.out.println("Isn't natural number.");
+        } else {
+            for (int i = 1; i <= num; i++) {
+                if (num % i == 0) {
+                    System.out.println(i + " is divider number " + num);
+                    int count = 1;
+
+                    for (int k = 1; k <=i/2; k++){
+                        if (i%k == 0){
+                            count++;
+                        }
+                    }
+
+                    if(count == 2){
+                        System.out.println("Is simple divider!\n");
+                    } else {
+                        System.out.println("But not simple.\n");
+                    }
+                }
+            }
         }
     }
 }

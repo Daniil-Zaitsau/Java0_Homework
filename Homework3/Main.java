92dc5990f3ec61bd8dcf53e4b5b752ef42b88a2a
Daniil Zaitsau <44928584+daden4@users.noreply.github.com>
2019-02-27 23:32:27 +0300

    Update and rename Matrix.java to Main.java

--- Homework3/Main.java
+++ Homework3/Main.java
@@ -1,7 +1,7 @@
 import java.util.Random;
 import java.util.Scanner;
 
-public class Matrix {
+public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
